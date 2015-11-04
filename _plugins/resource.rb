2fc4515bc573295306ec58c9e521772196452182
Santos <santos82h@gmail.com>
2015-11-04 20:12:07 +0100

    plugin de recursos

--- _plugins/resource.rb
+++ _plugins/resource.rb
@@ -27,21 +27,49 @@ module Jekyll
       super
       @text = text.strip
     end
+    
+    def get_attr(r,lang)
+    	foot=nil
+    	s=""
+    	if not r
+    		return s,foot
+    	end
+    	if lang and r[lang]
+			l=r[lang]
+    		%w[alt title parent longdesc].each do |a|
+				if l[a]
+					s=s+a+"=\""+l[a]+"\" ";
+				end
+    		end
+    		foot=l['foot']
+    	end
+    	
+		%w[height width].each do |a|
+			if r[a]
+				s=s+a+"=\""+r[a]+"\" ";
+			end
+		end
+		
+		return s,foot
+    end
 
     def render(context)
     	site = context.registers[:site]
+    	page = context.registers[:page]
     	f, r=get_file(site)
     	if f.nil?
     		return "<p>#{@text} not found!!</p>"
     	end
     	url = f.path.sub(site.source,"")
+    	
+    	att,foot=get_attr(r,page['lang'])
 
     	if f.extname =~ /^\.(svg|png|jpeg|jpg|gif)$/
-    		return "<img src=\"#{url}\" id=\"#{@text}\" title=\"\" alt=\"\" />"
+    		return "<img src=\"#{url}\" id=\"#{@text}\" "+att+"/>"
     	end
     	if f.extname =~ /^\.(mp4)$/
     		r = f.extname[1..-1]
-    		return "<video width=\"900\" controls id=\"#{@text}\"><source src=\"#{url}\" type=\"video/#{r}\"></video>"
+    		return "<video width=\"900\" controls id=\"#{@text}\" "+att+"><source src=\"#{url}\" type=\"video/#{r}\"></video>"
     	end
     	return "#{@text} is not a resource"
     end
