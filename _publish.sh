20ee35c128c84e99008ac1f1f17dc70e8645fbf3
Santos <santos82h@gmail.com>
2015-12-24 03:24:23 +0100

    publish in app engine

---
+++ _publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Build the _site/ contents with Jekyll
+jekyll build
+
+#~/source/google/google_appengine/appcfg.py --oauth2 update .
+~/source/google/google_appengine/appcfg.py update .
+
